efd49c2e09a915d78ab62cf01ce90c2e96b22d40
구민서 <minseo12379@gmail.com>
2025-10-18 21:35:32 +0900

    feat(Application): 커스텀 구분자 지정 기능 구현
    기본 구분자(',', ':') 외,
    문자열 앞부분 "//"와 "\n" 사이에 오는 문자를 구분자로 지정하는 기능 구현
    
    예시: "//;\n1,2:3;4" -> 10을 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,35 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        int sum = 0;
-
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = Console.readLine();
+
+        int sum = 0;
+        Pattern pattern = Pattern.compile("//(.)\\\\n(.+)");
+        Matcher matcher = pattern.matcher(inputString);
 
         if (inputString.isEmpty()) {
             sum = 0;
         } else {
-            String[] inputStringArr = inputString.split("[,|:]");
-            for (String string : inputStringArr) {
-                sum += Integer.parseInt(string);
+            if (matcher.matches()) {
+                String customDelimiter = matcher.group(1);
+                String stringToCheck = matcher.group(2);
+
+                String delimiter = Pattern.quote(customDelimiter) + "|,|:";
+                String[] digitArr = stringToCheck.split(delimiter);
+                for (String digit : digitArr) {
+                    sum += Integer.parseInt(digit);
+                }
+            } else {
+                String[] digitArr = inputString.split("[,|:]");
+                for (String digit : digitArr) {
+                    sum += Integer.parseInt(digit);
+                }
             }
         }
         System.out.println("결과 : " + sum);
