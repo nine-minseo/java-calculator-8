e42f6e8b247614e39356c98af03e01b1153e3341
구민서 <minseo12379@gmail.com>
2025-10-18 16:29:41 +0900

    feat(Application): 쉼표 또는 콜론을 구분자로 숫자 합산
    쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 입력받았을 떄,
    구분자를 기준으로 문자열을 분리하여 각 숫자의 합을 반환하는 기능 구현
    
    예시: "" -> 0 , "1,2" -> 3, "1,2:3" -> 6을 반환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int sum = 0;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
+
+        if (inputString.isEmpty()) {
+            sum = 0;
+        } else {
+            String[] inputStringArr = inputString.split("[,|:]");
+            for (String string : inputStringArr) {
+                sum += Integer.parseInt(string);
+            }
+        }
+        System.out.println("결과 : " + sum);
     }
 }
